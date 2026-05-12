6a8267bac0d74c4896391efda20d8fe498384cea
iasminsquiapati <squiapatii@gmail.com>
2026-05-12 10:15:17 -0300

    Apresentando dados na datatable

--- ctrl_home.py
+++ ctrl_home.py
@@ -21,7 +21,23 @@ def index(): # função que gerencia rota
 @bp.route("/dashboard") # cria uma rota para navegador http://127.0.0.1:5000/dashboard
 def dashboard(): # função que gerencia rota
     """ Painel de vendas"""
-    # if 'user' not in session:
-    #     return redirect(url_for("auth.login"))
+    #remova o login
     
-    return render_template("dashboard/index.html", title="Painel de Vendas") # Renderiza um template
+    vendas: list = [
+        {"mes":"Janeiro", "valortotal" :139519.19},
+        {"mes":"Fevereiro", "valortotal" :149629.18},
+        {"mes":"Março", "valortotal" :159989.16},
+        {"mes":"Abril", "valortotal" :169419.19},
+        {"mes":"Maio", "valortotal" :179599.17},
+        {"mes":"Junho", "valortotal" :189439.19},
+        {"mes":"Julho", "valortotal" :199519.20},
+        {"mes":"Agosto", "valortotal" :239329.17},
+        {"mes":"Setembro", "valortotal" :249509.19},
+        {"mes":"Outubro", "valortotal" :259539.18},
+        {"mes":"Novembro", "valortotal" :269710.19},
+        {"mes":"Dezembro", "valortotal" :279815.20},
+
+
+    ] #fim lista vendas
+    
+    return render_template("dashboard/index.html", title="Painel de Vendas", vendas=vendas) # Renderiza um template
