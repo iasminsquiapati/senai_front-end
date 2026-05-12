9922657516962fa2c19c7e9bcfe1d3cb5c850efb
iasminsquiapati <squiapatii@gmail.com>
2026-05-12 13:12:38 -0300

    Apresentando dados na datatable

--- ctrl_home.py
+++ ctrl_home.py
@@ -13,31 +13,32 @@ bp = Blueprint(__name__, "HomeController")
 @bp.route("/") # cria uma rota
 def index(): # função que gerencia rota
     """ Página inicial"""
-    if 'user_id' not in session:
-         return redirect(url_for("auth.login"))
+    if '_user_id' not in session:
+        return redirect(url_for("auth.login"))
     
     return render_template("dashboard/index.html") # Renderiza um template
-    
+
+
 @bp.route("/dashboard") # cria uma rota para navegador http://127.0.0.1:5000/dashboard
-def dashboard(): # função que gerencia rota
+def dashboard(): # função que gerencia rota deve ser única
     """ Painel de vendas"""
-    #remova o login
-    
-    vendas: list = [
-        {"mes":"Janeiro", "valortotal" :139519.19},
-        {"mes":"Fevereiro", "valortotal" :149629.18},
-        {"mes":"Março", "valortotal" :159989.16},
-        {"mes":"Abril", "valortotal" :169419.19},
-        {"mes":"Maio", "valortotal" :179599.17},
-        {"mes":"Junho", "valortotal" :189439.19},
-        {"mes":"Julho", "valortotal" :199519.20},
-        {"mes":"Agosto", "valortotal" :239329.17},
-        {"mes":"Setembro", "valortotal" :249509.19},
-        {"mes":"Outubro", "valortotal" :259539.18},
-        {"mes":"Novembro", "valortotal" :269710.19},
-        {"mes":"Dezembro", "valortotal" :279815.20},
+    # remova o login
+    import locale
+    # define a localização para portugues brasileiro 
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+    vendas : list = [
+         {"mes" :"janeiro", "total":139519.19 },
+         {"mes" :"fevereiro", "total":149519.19 },
+         {"mes" :"Março", "total":159519.19 },
+         {"mes" :"abril", "total":169519.19 },
+         {"mes" :"maio", "total":179519.19 },
+         {"mes" :"junho", "total":189519.19 },
+         {"mes" :"julho", "total":199519.19 },           
+         {"mes" :"agosto", "total":20519.19 },
+         {"mes" :"setembro", "total":219519.19 },
+         {"mes" :"outubro", "total":229519.19 },
+         {"mes" :"novembro", "total":239519.19 },
+         {"mes" :"dezembro", "total":249519.19 },
+    ] #fim lista vendas           
 
-
-    ] #fim lista vendas
-    
-    return render_template("dashboard/index.html", title="Painel de Vendas", vendas=vendas) # Renderiza um template
+    return render_template("dashboard/index.html" , title="Painel de vendas", vendas=vendas, locale=locale) # Renderiza um template
